0446914701e7e4974e3792ed86b19193a7342b7f
DragoonWise <dragoonwise@hotmail.com>
2020-02-25 09:23:28 +0100

    importexport

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,10 +6,22 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import '../css/app.scss';
+import "../css/app.scss";
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-import $ from 'jquery';
+import $ from "jquery";
 
-require('bootstrap');
+require("bootstrap");
 // console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+
+$(function() {
+  $("#exportform").submit(function(e) {
+      e.preventDefault();
+      // Export du fichier
+
+  });
+  $("#importform").submit(function(e) {
+    e.preventDefault();
+    // importanalyse
+});
+});
